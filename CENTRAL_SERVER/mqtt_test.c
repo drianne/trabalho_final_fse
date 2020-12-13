97699b583efdf18ce29c086cd9ce86a58fb01330
Matheus Roberto <as.matheusroberto@gmail.com>
2020-12-13 12:45:43 -0300

    Adding mqtt configuration to rasp
    Co-authored-by: drianne <driah_as@hotmail.com>

---
+++ CENTRAL_SERVER/mqtt_test.c
@@ -0,0 +1,62 @@
+// gcc mqtt_test.c -o mqtt_test -lpaho-mqtt3c -lcjson -Wall
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <MQTTClient.h>
+#include <cjson/cJSON.h>
+
+#define MQTT_ADDRESS   "tcp://mqtt.eclipseprojects.io:1883"
+#define CLIENTID       "MQTTCClientID_TESTE"  
+
+#define MQTT_PUBLISH_TOPIC     "servidor/resposta"
+#define MQTT_SUBSCRIBE_TOPIC   "sensores/temperatura"
+ 
+MQTTClient client;
+ 
+void publish(MQTTClient client, char* topic, char* payload);
+int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message);
+ 
+void publish(MQTTClient client, char* topic, char* payload) {
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+ 
+    pubmsg.payload = payload;
+    pubmsg.payloadlen = strlen(pubmsg.payload);
+    pubmsg.qos = 2;
+    pubmsg.retained = 0;
+    MQTTClient_deliveryToken token;
+    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+    MQTTClient_waitForCompletion(client, token, 1000L);
+}
+ 
+int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    char* payload = message->payload;
+    printf("Mensagem recebida! \n\rTopico: %s Mensagem: %s\n", topicName, payload);
+ 
+    publish(client, MQTT_PUBLISH_TOPIC, payload);
+ 
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName); 
+    return 1;
+}
+ 
+int main(int argc, char *argv[])
+{
+   int rc;
+   MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+ 
+   MQTTClient_create(&client, MQTT_ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+   MQTTClient_setCallbacks(client, NULL, NULL, on_message, NULL);
+ 
+   rc = MQTTClient_connect(client, &conn_opts);
+  
+   if (rc != MQTTCLIENT_SUCCESS)
+   {
+       printf("\n\rFalha na conexao ao broker MQTT. Erro: %d\n", rc);
+       exit(-1);
+   }
+ 
+   MQTTClient_subscribe(client, MQTT_SUBSCRIBE_TOPIC, 0);
+ 
+   while(1){}
+}
--- CLIENT/main/sensor_module.c
+++ CLIENT/main/sensor_module.c
@@ -11,16 +11,8 @@ void get_temp_humidity(){
     DHT11_init(GPIO_NUM_4);
 
     while(1) {
-        printf("Reading...\n");
         last_read = DHT11_read();
 
-        if(last_read.temperature > -1){
-          printf("Temperature is %d \n", last_read.temperature);
-          printf("Humidity is %d\n", last_read.humidity);
-          printf("Status code is %d\n", last_read.status);
-        }else{
-          printf(">> Error\n");
-        }      
         vTaskDelay(1);
     }
 }
